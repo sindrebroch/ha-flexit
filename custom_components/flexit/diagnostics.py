9ea77c07745c4e4d24b6c1242138091a90f2bacb
Sindre Broch <sindrebroch@gmail.com>
2022-02-16 18:37:40 +0100

    Add skeleton diagnostics

---
+++ custom_components/flexit/diagnostics.py
@@ -0,0 +1,23 @@
+"""Diagnostics support for Flexit."""
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from custom_components.flexit.api import FlexitApiClient
+
+from custom_components.flexit.coordinator import FlexitDataUpdateCoordinator
+from custom_components.flexit.models import Entity, FlexitSensorsResponse
+
+from .const import DOMAIN
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, config_entry: ConfigEntry
+) -> dict:
+    """Return diagnostics for a config entry."""
+
+    coordinator: FlexitDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
+    api: FlexitApiClient = coordinator.api
+    data: FlexitSensorsResponse = coordinator.data
+
+    return dict()
