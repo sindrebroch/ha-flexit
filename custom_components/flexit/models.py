7902fe1c3c9730abe77fb5b5eadaa5ee122c74d2
Sindre Broch <sindre.broch@nav.no>
2022-02-18 10:37:40 +0100

    Cleanup

--- custom_components/flexit/models.py
+++ custom_components/flexit/models.py
@@ -12,8 +12,6 @@ from .const import (
     AWAY_AIR_TEMPERATURE_PATH,
     AWAY_DELAY_PATH,
     BOOST_DURATION_PATH,
-    CURRENT_BOOST_DURATION_PATH,
-    CURRENT_FIREPLACE_DURATION_PATH,
     DEVICE_DESCRIPTION_PATH,
     FIREPLACE_DURATION_PATH,
     HEATER_PATH,
@@ -100,35 +98,35 @@ class UtilClass:
         self.data: Dict[str, Any] = data
         self.plant: str = plant
 
-    def _str_device(self, path: str) -> str:
+    def str_device(self, path: str) -> str:
         """Get value from path."""
         return self.data[VALUES][f"{self.plant}{path}"][VALUE]
 
-    def _int_device(self, path: str) -> int:
+    def int_device(self, path: str) -> int:
         """Get int from path."""
-        return int(self._str_device(path))
+        return int(self.str_device(path))
 
     def _str_sensor(self, path: str) -> str:
         """Get string from path."""
         return self.data[VALUES][f"{self.plant}{path}"][VALUE][VALUE]
 
-    def _int_sensor(self, path: str) -> int:
+    def int_sensor(self, path: str) -> int:
         """Get int from path."""
         return int(self._str_sensor(path))
 
-    def _float_sensor(self, path: str) -> float:
+    def float_sensor(self, path: str) -> float:
         """Get float from path."""
         return round(float(self._str_sensor(path)), 2)
 
-    def _dirty_filter(self, operating_time: int, change_interval: int) -> bool:
+    def dirty_filter(self, operating_time: int, change_interval: int) -> bool:
         """Get filter status based on hours operated."""
         return True if operating_time >= change_interval else False
 
-    def _is_heating(self, heater_int: int) -> bool:
+    def is_heating(self, heater_int: int) -> bool:
         """Get electric heater status from integer."""
         return True if heater_int == 1 else False
 
-    def _ventilation_mode(self, ventilation_int: int) -> str:
+    def ventilation_mode(self, ventilation_int: int) -> str:
         """Get ventilation mode from integer."""
 
         # Null*Off*Away*Home*High*Cocker hood*Fire place*Forced ventilation
@@ -212,34 +210,32 @@ class FlexitSensorsResponse:
         util = UtilClass(data=data, plant=plant)
 
         return FlexitSensorsResponse(
-            home_air_temperature=util._float_sensor(HOME_AIR_TEMPERATURE_PATH),
-            away_air_temperature=util._float_sensor(AWAY_AIR_TEMPERATURE_PATH),
-            outside_air_temperature=util._float_sensor(OUTSIDE_AIR_TEMPERATURE_PATH),
-            supply_air_temperature=util._float_sensor(SUPPLY_AIR_TEMPERATURE_PATH),
-            exhaust_air_temperature=util._float_sensor(EXHAUST_AIR_TEMPERATURE_PATH),
-            extract_air_temperature=util._float_sensor(EXTRACT_AIR_TEMPERATURE_PATH),
-            room_temperature=util._float_sensor(ROOM_TEMPERATURE_PATH),
-            electric_heater=util._is_heating(util._int_sensor(HEATER_PATH)),
-            ventilation_mode=util._ventilation_mode(util._int_sensor(MODE_PATH)),
-            filter_operating_time=util._int_sensor(FILTER_OPERATING_TIME_PATH),
-            filter_time_for_exchange=util._int_sensor(FILTER_TIME_FOR_EXCHANGE_PATH),
-            dirty_filter=util._dirty_filter(
-                util._int_sensor(FILTER_OPERATING_TIME_PATH),
-                util._int_sensor(FILTER_TIME_FOR_EXCHANGE_PATH),
+            home_air_temperature=util.float_sensor(HOME_AIR_TEMPERATURE_PATH),
+            away_air_temperature=util.float_sensor(AWAY_AIR_TEMPERATURE_PATH),
+            outside_air_temperature=util.float_sensor(OUTSIDE_AIR_TEMPERATURE_PATH),
+            supply_air_temperature=util.float_sensor(SUPPLY_AIR_TEMPERATURE_PATH),
+            exhaust_air_temperature=util.float_sensor(EXHAUST_AIR_TEMPERATURE_PATH),
+            extract_air_temperature=util.float_sensor(EXTRACT_AIR_TEMPERATURE_PATH),
+            room_temperature=util.float_sensor(ROOM_TEMPERATURE_PATH),
+            electric_heater=util.is_heating(util.int_sensor(HEATER_PATH)),
+            ventilation_mode=util.ventilation_mode(util.int_sensor(MODE_PATH)),
+            filter_operating_time=util.int_sensor(FILTER_OPERATING_TIME_PATH),
+            filter_time_for_exchange=util.int_sensor(FILTER_TIME_FOR_EXCHANGE_PATH),
+            dirty_filter=util.dirty_filter(
+                util.int_sensor(FILTER_OPERATING_TIME_PATH),
+                util.int_sensor(FILTER_TIME_FOR_EXCHANGE_PATH),
             ),
-            heat_exchanger_speed=util._int_sensor(HEAT_EXCHANGER_SPEED_PATH),
-            supply_fan_speed=util._int_sensor(SUPPLY_FAN_SPEED_PATH),
-            supply_fan_control_signal=util._int_sensor(SUPPLY_FAN_CONTROL_SIGNAL_PATH),
-            extract_fan_speed=util._int_sensor(EXTRACT_FAN_SPEED_PATH),
-            extract_fan_control_signal=util._int_sensor(
-                EXTRACT_FAN_CONTROL_SIGNAL_PATH
-            ),
-            additional_heater=util._int_sensor(ADDITIONAL_HEATER_PATH),
-            alarm_code_a=util._int_sensor(ALARM_CODE_A_PATH),
-            alarm_code_b=util._int_sensor(ALARM_CODE_B_PATH),
-            fireplace_duration=util._int_sensor(FIREPLACE_DURATION_PATH),
-            boost_duration=util._int_sensor(BOOST_DURATION_PATH),
-            away_delay=util._int_sensor(AWAY_DELAY_PATH),
+            heat_exchanger_speed=util.int_sensor(HEAT_EXCHANGER_SPEED_PATH),
+            supply_fan_speed=util.int_sensor(SUPPLY_FAN_SPEED_PATH),
+            supply_fan_control_signal=util.int_sensor(SUPPLY_FAN_CONTROL_SIGNAL_PATH),
+            extract_fan_speed=util.int_sensor(EXTRACT_FAN_SPEED_PATH),
+            extract_fan_control_signal=util.int_sensor(EXTRACT_FAN_CONTROL_SIGNAL_PATH),
+            additional_heater=util.int_sensor(ADDITIONAL_HEATER_PATH),
+            alarm_code_a=util.int_sensor(ALARM_CODE_A_PATH),
+            alarm_code_b=util.int_sensor(ALARM_CODE_B_PATH),
+            fireplace_duration=util.int_sensor(FIREPLACE_DURATION_PATH),
+            boost_duration=util.int_sensor(BOOST_DURATION_PATH),
+            away_delay=util.int_sensor(AWAY_DELAY_PATH),
         )
 
 
@@ -266,17 +262,17 @@ class FlexitDeviceInfo:
         util = UtilClass(data=data, plant=plant)
 
         return FlexitDeviceInfo(
-            fw=util._str_device(FIRMWARE_REVISION_PATH),
-            modelName=util._str_device(MODEL_NAME_PATH),
-            modelInfo=util._str_device(MODEL_INFORMATION_PATH),
-            serialInfo=util._str_device(SERIAL_NUMBER_PATH),
-            systemStatus=util._str_device(SYSTEM_STATUS_PATH),
-            status=util._str_device(OFFLINE_ONLINE_PATH),
-            deviceDescription=util._str_device(DEVICE_DESCRIPTION_PATH),
-            applicationSoftwareVersion=util._str_device(
+            fw=util.str_device(FIRMWARE_REVISION_PATH),
+            modelName=util.str_device(MODEL_NAME_PATH),
+            modelInfo=util.str_device(MODEL_INFORMATION_PATH),
+            serialInfo=util.str_device(SERIAL_NUMBER_PATH),
+            systemStatus=util.str_device(SYSTEM_STATUS_PATH),
+            status=util.str_device(OFFLINE_ONLINE_PATH),
+            deviceDescription=util.str_device(DEVICE_DESCRIPTION_PATH),
+            applicationSoftwareVersion=util.str_device(
                 APPLICATION_SOFTWARE_VERSION_PATH
             ),
-            lastRestartReason=util._int_device(LAST_RESTART_REASON_PATH),
+            lastRestartReason=util.int_device(LAST_RESTART_REASON_PATH),
         )
 
 
